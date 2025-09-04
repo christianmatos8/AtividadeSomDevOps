6fcf56bbcc9207ee4cf1bb073d48eec5082de04c
christianmatos8 <cristianmatos8@gmail.com>
2025-09-04 20:10:06 -0300

    criação-de-categorias
    Neste commit, foi adicionado a função de escolher uma categorias aleatória para o nome criado pelo código.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import random
 
+categorias = ["Mago", "Guerreiro", "Paladino", "Elfo", "Bardo"]
 prefixos = ["Zan", "Mor", "El", "Thar", "Gal",]
 sufixos = ["dor", "ion", "mir", "thas", "wyn",]
 
 def gerar_nome():
-
     return random.choice(prefixos) + random.choice(sufixos)
+
+def escolher_categoria():
+    return random.choice(categorias)
 
 def main():
     print("Seu nome fantástico é:", gerar_nome())
+    print("você pertence a categoria:", escolher_categoria())
 
 if __name__ == "__main__":
     main()
